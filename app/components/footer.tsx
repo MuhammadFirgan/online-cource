87d2c31ea4734395180fdc20cd22ff18af4380bb
muhammad.firgan <muhammadfirgan50@gmail.com>
2023-10-10 21:48:26 +0700

    footer (belum beres)

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -11,15 +11,40 @@ export const caveat_brush : NextFont = Caveat_Brush({
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col justify-center items-center px-4 md:px-14 md:flex-row">
-        <div>
+    <footer className="flex flex-col justify-center items-center px-4 py-5 md:px-14 md:flex-row">
+        <div className="flex flex-col items-center gap-2">
             <div className="flex items-center gap-1">
                 <FaComputer className="text-2xl text-emerald-500" />
                 <h1 className={`text-3xl text-black ${caveat_brush.className}`}>C4U</h1>
-                <div className="flex">
-                    <Link />
-                </div>
             </div>
+            <div className="flex gap-4 text-2xl text-gray-500">
+                <Link href='/'>
+                  <FaInstagram />
+                </Link>
+                <Link href='/'>
+                  <FaYoutube/>
+                </Link>
+                <Link href='/'>
+                  <FaFacebook/>
+                </Link>
+                <Link href='/'>
+                  <FaGithub/>
+                </Link>
+            </div>
+        </div>
+        <div>
+          {/* <ul>
+          {footerLinks.map((footerLink, i) => (
+            <li>{footerLink.title}</li>
+          ))}
+          </ul>
+          <ul>
+          {footerLinks.links.map((link, i) => (
+            <li key={i}>{link.title}</li>
+              
+          ))}
+
+          </ul> */}
         </div>
     </footer>
   )
--- app/constans/index.ts
+++ app/constans/index.ts
@@ -10,21 +10,13 @@ export const footerLinks = [
         ],
     },
     {
-        title: "Popular Technology",
-        links: [
-          { title: "React", url: "/" },
-          { title: "Laravel", url: "/" },
-          { title: "Linux", url: "/" },
-          { title: "Tailwind CSS", url: "/" },
-        ],
-    },
-    {
         title: "Popular Language",
         links: [
           { title: "HTML & CSS", url: "/" },
           { title: "Javascript", url: "/" },
           { title: "PHP", url: "/" },
           { title: "Python", url: "/" },
+          { title: "Flutter", url: "/" },
         ],
     },
 ]
